da9994ba0e7d015f713e0c0457e7e006ec5a526c
bagindaisfa <bagindaisfa05@gmail.com>
2022-11-27 18:39:01 +0700

    remove unused files

--- src/Config/helper/zustand.js
+++ src/Config/helper/zustand.js
@@ -1,6 +1,1 @@
-import create from 'zustand'
-
-export const updateJournalUmumContext = create( set => ({
-    updateJournalUmumStore : null,
-    setUpdateJournalUmumStore : param => set (() => ({updateJournalUmumStore:param}))
-  }))
+import create from "zustand";
